25deb3b8a3a2e6f1d45b8d4273c4711eb105ec5b
Paul Crane <paul@acheronsoft.com>
2020-12-14 00:27:00 -0600

    Add Collection type, Collection.Count and NewAddons to addons.go.

--- addons/addons.go
+++ addons/addons.go
@@ -1,1 +1,18 @@
 package addon
+
+// Collection A collection of Addon objects
+type Collection struct {
+	addons []Addon
+}
+
+// Count Get the number of intstalled addons
+func (collection Collection) Count() int {
+	return len(collection.addons)
+}
+
+// NewAddons create a new addon instance
+func NewAddons() Collection {
+	var addons Collection
+
+	return addons
+}
